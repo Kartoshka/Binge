dd786f20e867c6e0bd0c763e3adc571726fed681
Kartoshka <eli.harfouche@gmail.com>
2016-11-19 14:58:58 -0500

    Update adding to liked list. Add Search bar and search function

--- Model.js
+++ Model.js
@@ -1,5 +1,7 @@
+
 var likedMoviesListName = 'LikedMovies';
 var dislikedMoviesListName = 'DislikedMovies';
+var emptyFunc = function(){};
 
 function getMovieInfo(id, callback){
 	$.getJSON( "https://api.themoviedb.org/3/tv/"+id+"?api_key=6bca0b74270a3299673d934c1bb11b4d&language=en-US", callback);
@@ -9,8 +11,12 @@ function getSuggestions(id,callback){
 	$.getJSON( "https://api.themoviedb.org/3/tv/"+id+"/similar?api_key=6bca0b74270a3299673d934c1bb11b4&language=en-US", callback);
 }
 
+var results;
 function getMovies(name, callback){
-	$.getJSON("https://api.themoviedb.org/3/search/tv?api_key=6bca0b74270a3299673d934c1bb11b4d&language=en-US&query="+name,callback);
+	if(results){
+		results.abort();
+	}
+	results= $.getJSON("https://api.themoviedb.org/3/search/tv?api_key=6bca0b74270a3299673d934c1bb11b4d&language=en-US&query="+name,callback);
 }
 
 function populateRecommended(allMovies,evaluate)
@@ -35,18 +41,19 @@ function addMovieToList(id, rating)
 	}
 }
 
-function addMovieToList(id, rating, listName)
+function addMovieToList(id, rating, listName, callback)
 {
 	if(!isNaN(id) && id >= 0){ 
 		var movieList = getMovieList(listName);
-		console.log(movieList.length);
-		if(movieList.indexOf(id) == -1){
+		var index = indexOfID(movieList,id);
+
+		if(index == -1){
 			getMovieInfo(id,function (data) {
 				if(!data.status_code)
 				 {
 					movieList.push({id:data.id, genres:data.genres, first_air_date:data.first_air_date, origin_country:data.origin_country, popularity:data.popularity, vote_average:data.vote_average, app_user_rating:rating});
 					saveMovieList(movieList,listName);
-
+					callback(id);
 				}
 			});
 		}
@@ -54,7 +61,7 @@ function addMovieToList(id, rating, listName)
 	}
 }
 
-function removeMovieFromList(id, listName)
+function removeMovieFromList(id, listName, callback)
 {
 	var rating =1;
 	if(!isNaN(id) && id >= 0){
@@ -64,7 +71,7 @@ function removeMovieFromList(id, listName)
 		if(index != -1){
 			movieList.splice(index,1);
 		}
-		saveMovieList(movieList,listName);
+		saveMovieList(movieList,listName,callback);
 	}
 }
 
@@ -76,13 +83,18 @@ function getMovieList(listName)
 	return JSON.parse(localStorage.getItem(listName));
 }
 
-function saveMovieList(movieList,listName){
+function saveMovieList(movieList,listName,callback){
+	console.log(3);
+
+	callback = callback || emptyFunc;
+
 	localStorage.setItem(listName,JSON.stringify(movieList));
+	callback();
 }
 
-function clearMovieList(listName){
+function clearMovieList(listName,callback){
 	var movieList = [];
-	saveMovieList(movieList,listName);
+	saveMovieList(movieList,listName,callback);
 }
 
 function movieInList(id,listName){
@@ -90,20 +102,21 @@ function movieInList(id,listName){
 	return indexOfID(movieList,id) != -1;
 }
 
-function saveLikedMovies(likedMovies){
-	localStorage.setItem('LikedMovies',JSON.stringify(likedMovies));
+function saveLikedMovies(likedMovies,callback){
+	saveMovieList(likedMovies,likedMoviesListName,callback);
 }
 
-function addLikedMovie(id){
-	addMovieToList(id,1,likedMoviesListName);
+function addLikedMovie(id,callback){
+	console.log(1);
+	addMovieToList(id,1,likedMoviesListName,callback);
 }
 
-function removeLikedMovie(id){
-	removeMovieFromList(id,likedMoviesListName);
+function removeLikedMovie(id,callback){
+	removeMovieFromList(id,likedMoviesListName,callback);
 }
 
-function clearLikedMovies(){
-	clearMovieList(likedMoviesListName);
+function clearLikedMovies(callback){
+	clearMovieList(likedMoviesListName,callback);
 }
 
 function movieIsLiked(id){
@@ -152,4 +165,5 @@ function indexOfID(array,id){
 
 	return -1;
 }
+
 
--- Movie.js
+++ Movie.js
@@ -1,17 +1,25 @@
-function Movie(id, container) {
+function Movie(id, container,data) {
     this.id = id;
     this.container = container;
 
     var self = this;
 
-    getMovieInfo(id, function (data) {
+    function createNewMovie(data) {
         self.data = data;
-        console.log(data);
-
         self.element = $('<div>').addClass('movie');
         self.image = $('<img>').appendTo(self.element).attr('src', "http://image.tmdb.org/t/p/w1000" + data.poster_path).addClass('movie-thumbnail').attr('title', data.name);
         // self.title = $('<h4>').appendTo(self.element).text(data.name).addClass('movie-title');
 
         self.container.append(self.element);
-    });
+    }
+
+    if(data)
+    {
+        createNewMovie(data);
+    }
+    else
+    {
+        getMovieInfo(id, createNewMovie);
+    }
 }
+
