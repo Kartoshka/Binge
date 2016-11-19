3ef3b5b0de7649aaff925293a9898924cf460f82
Mathieu Bolduc <mbolduc567@gmail.com>
2016-11-19 17:05:00 -0500

    Fix: Multipage pooling part 2

--- KNN.js
+++ KNN.js
@@ -1,57 +1,62 @@
-function getDistance(movie1, movie2) {
-    var distance = 0,
-        tempDist = 0;
+function KNN(numRecommendations, numPages) {
 
-    // genres
-    var commonGenres = 1;
-    for (var i = 0; i < movie1.genre_ids.length; i++) {
-        for (var j = 0; j < movie2.genre_ids.length; j++) {
-            if (movie1.genre_ids[i] === movie2.genre_ids[j])
-                commonGenres++;
+    this.n = numRecommendations;
+    this.numPages = numPages;
+    this.recommendations = [];
+    this.ratings = [];
+
+    function getDistance(movie1, movie2) {
+        var distance = 0,
+            tempDist = 0;
+
+        // genres
+        var commonGenres = 1;
+        for (var i = 0; i < movie1.genre_ids.length; i++) {
+            for (var j = 0; j < movie2.genre_ids.length; j++) {
+                if (movie1.genre_ids[i] === movie2.genre_ids[j])
+                    commonGenres++;
+            }
         }
-    }
-    tempDist = 20 / commonGenres;
-    distance += tempDist * tempDist;
+        tempDist = 100 / commonGenres;
+        distance += tempDist;
 
-    // other fields
-    var fields = ["first_air_date", "popularity", "vote_average"];
-    for (var i = 0; i < fields.length; i++) {
-        tempDist = Math.abs(parseFloat(movie1[fields[i]]) - parseFloat(movie2[fields[i]]));
+        // other fields
+
+        tempDist = Math.abs(parseFloat(movie1.first_air_date) - parseFloat(movie2.first_air_date));
         distance += tempDist * tempDist;
+
+        distance = Math.sqrt(distance);
+        return distance;
     }
 
-    distance = Math.sqrt(distance);
-    return distance;
-}
+    function getCloseness(movie, likedMovies) {
 
-function getCloseness(movie, likedMovies) {
-
-    var closeness = 0;
-    for (var i = 0; i < likedMovies.length; i++) {
-        closeness += 1 / getDistance(likedMovies[i], movie);
-    }
-    return closeness;
-}
-
-// returns the n best recommendations from moviePool, in order of relevance
-function getRecommended(moviePool, n) {
-
-    var likedMovies = getLikedMovies();
-    if (!likedMovies) {
-        return moviePool.slice(0, n);
-    }
-
-    var recommendations = [];
-    var ratings = [];
-    for (var i = 0; i < moviePool.length; i++) {
-        var rating = getCloseness(moviePool[i], likedMovies);
-        var j = 0;
-        while (j < Math.min(ratings.length, n) && ratings[j] > rating) {
-            j++;
+        var closeness = 0;
+        for (var i = 0; i < likedMovies.length; i++) {
+            closeness += 1 / getDistance(likedMovies[i], movie);
         }
-        recommendations.splice(j, 0, moviePool[i]);
-        ratings.splice(j, 0, rating);
+        return closeness;
     }
 
-    return recommendations.slice(0, n);
+    // returns the n best recommendations from moviePool, in order of relevance
+    this.updateRecommendations = function (moviePool) {
+
+        var likedMovies = getLikedMovies();
+        if (!likedMovies) {
+            return moviePool.slice(0, this.n);
+        }
+
+        for (var i = 0; i < moviePool.length; i++) {
+            var rating = getCloseness(moviePool[i], likedMovies);
+            var j = 0;
+            while (j < Math.min(this.ratings.length, this.n) && this.ratings[j] > rating) {
+                j++;
+            }
+            this.recommendations.splice(j, 0, moviePool[i]);
+            this.ratings.splice(j, 0, rating);
+        }
+
+        this.recommendations = this.recommendations.slice(0, this.n);
+        this.ratings = this.ratings.slice(0, this.n);
+    }
 }
