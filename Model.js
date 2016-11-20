695e64d96094667ff653f84706a7bdcf9a5e8a08
Kartoshka <eli.harfouche@gmail.com>
2016-11-19 21:18:44 -0500

    actually remove functions from model and update index

--- Model.js
+++ Model.js
@@ -99,38 +99,6 @@ function movieInList(id, listName) {
     return indexOfID(movieList, id) != -1;
 }
 
-function saveLikedMovies(likedMovies, callback) {
-    saveMovieList(likedMovies, likedMoviesListName, callback);
-}
-
-function addLikedMovie(id, rating,callback) {
-    addMovieToList(id, rating, likedMoviesListName, callback);
-}
-
-function removeLikedMovie(id, callback) {
-    removeMovieFromList(id, likedMoviesListName, callback);
-}
-
-function clearLikedMovies(callback) {
-    clearMovieList(likedMoviesListName, callback);
-}
-
-function movieIsLiked(id) {
-    return movieInList(likedMoviesListName);
-}
-
-function saveLikedMovies(likedMovies) {
-    saveMovieList(likedMoviesListName);
-}
-
-function getLikedMovies() {
-    return getMovieList(likedMoviesListName);
-}
-
-function retrieveLikedMovies(container) {
-    container.append(makeUL(getLikedMovies()));
-
-}
 function makeUL(array) {
     // Create the list element:
     var list = document.createElement('ul');
